0ebf8955ee6f06863fe90ed0a898b466219cef17
NightfuryEquinn <xianzyip8@gmail.com>
2023-03-31 16:41:36 +0800

    Data tabulating

--- script.js
+++
@@ -1,8 +0,0 @@
-$('.accordian').on('click', '.accordian-control', function(e) {
-  e.preventDefault();
-
-  $(this)
-    .next('.accordian-content')
-    .not(':animated')
-    .slideToggle(400);
-})
